fbca69d5538b482e417f1a3d6aa29c752b3154d8
Alessandro G. Oliveira <152395934+AlessandroG14@users.noreply.github.com>
2024-11-28 15:48:31 +0000

    Paragrafo adicionado

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ function Home(){
     return (
     <div><h1>E ai pepuda? Sorria :)</h1>
             <h2>Você merece um dia incrivel</h2>
+            <p>Vamos brincar</p>
+            <p>Hoje vamos andar de patins</p>
     </div>
 )
 }

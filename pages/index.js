de6b2883f22cb5ec3e4ee8e1ada40f45f6d9998f
Alessandro G. Oliveira <152395934+AlessandroG14@users.noreply.github.com>
2024-11-28 15:38:03 +0000

    Uma nova frase foi adicionada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home(){
-    return <h1>E ai pepuda? Sorria :)</h1>
+    return (
+    <div><h1>E ai pepuda? Sorria :)</h1>
+            <h2>Você merece um dia incrivel</h2>
+    </div>
+)
 }
 
 export default Home
